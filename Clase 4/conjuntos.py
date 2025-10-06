4b7bfd7aff35cac1f8139ef72acb51daec0ef0f0
pawlukmatias-cmd <pawlukmatias@gmail.com>
2025-10-06 11:11:06 -0300

    actualizacion

---
+++ Clase 4/conjuntos.py
@@ -0,0 +1,7 @@
+# Conjuntos
+frutas = {"manzana", "pera", "manzana", "naranja", "manzana", "manzana"}
+
+frutas.add("frutilla")
+frutas.discard("sandia")
+frutas.pop
+
---
+++ Clase 4/lista_compras.py
@@ -0,0 +1,10 @@
+# Lista de compras
+lista_compras = []
+
+# Añadimos productos
+lista_compras.append("Arroz")
+lista_compras.append("Fideos")
+lista_compras.append("Arvejas")
+lista_compras.pop()
+
+print("Esta es tu lista de compras: ", len(lista_compras))
