442f73fcbdfa18b92e7b11f74cc55e0e82eeb241
pawlukmatias-cmd <pawlukmatias@gmail.com>
2025-10-06 11:08:39 -0300

    añadimos suma

---
+++ Clase 4/suma.py
@@ -0,0 +1,9 @@
+# Vamos a sumar 2 numeros que el usuario elija:
+in_num1 = input("Ingrese primer número: ")
+num1 = int(in_num1)
+in_num2 = input("Ingrese segundo número: ")
+num2 = int(in_num2)
+
+suma = num1 + num2
+
+print(f"El resultado de sumar ", num1, " y ", num2, " es igual a ", suma)
